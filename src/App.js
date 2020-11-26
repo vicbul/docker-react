697fd1a5a4d3445c6789c70c163cad67f1486a1c
vbulnes <vbulnes@cisco.com>
2020-11-26 11:29:00 +0100

    text changed

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. What the hell?
+          Edit <code>src/App.js</code> and save to reload. Changed text!
         </p>
         <a
           className="App-link"
